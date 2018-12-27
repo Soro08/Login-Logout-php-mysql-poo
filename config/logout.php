7dc7c210c9afb28d92480e7838943c750fe16bbf
Soro08 <soro@nan.ci>
2018-12-28 00:59:29 +0100

    Tutoriel sur le systeme login et logout php-Mysql POO

---
+++ config/logout.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+    unset($_SESSION['id']);
+    header("location: ../index.php");
+    exit;
+
+
+?>
---
+++ config/verifier.php
@@ -0,0 +1,10 @@
+
+<?php
+
+if(!(isset($_SESSION['id'])) )
+{
+    header("location: ../index.php");
+    exit;
+}
+
+?>
